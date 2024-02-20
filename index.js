a4ebb64298c49c13e8c7693e7fe652c264c4693b
Manish Bhurtel <bhmanish8848@gmail.com>
2024-02-20 12:23:32 +0545

    UI refined

--- index.js
+++ index.js
@@ -152,12 +152,13 @@ const searchFunction = (userArray) => {
                            <div class="user-details-field">Phone : <span class="display-number">${user.user_phone}</span></div>
                         </div>
                            <div class="profile-format">
-                             <img src="./icons/edit-user.png" class="edit-single-profile icon" onclick="editUserProfile(${index}, '${escape(JSON.stringify(user))}')">
-                             <img src="./icons/delete-user.png" class="delete-single-profile icon" onclick="deleteUserProfile(${index})">
+                           <button class="edit-single-profile" onclick="editUserProfile(${index}, '${escape(JSON.stringify(user))}')">Edit Profile</button>
+        
+        
+                             <button class="delete-single-profile" onclick="deleteUserProfile(${index})">Delete Profile</button>
                            </div>
                         </div>
                     </div>`;
-        
                 finalData += userProfile;
             });
             container.innerHTML = finalData;
@@ -201,8 +202,8 @@ const displayUser = () => {
                    <div class="user-details-field">Phone : <span class="display-number">${user.user_phone}</span></div>
                 </div>
                    <div class="profile-format">
-                     <img src="./icons/edit-user.png" class="edit-single-profile icon" onclick="editUserProfile(${index}, '${escape(JSON.stringify(user))}')">
-                     <img src="./icons/delete-user.png" class="delete-single-profile icon" onclick="deleteUserProfile(${index})">
+                   <button class="edit-single-profile" onclick="editUserProfile(${index}, '${escape(JSON.stringify(user))}')">Edit Profile</button>
+                     <button class="delete-single-profile" onclick="deleteUserProfile(${index})">Delete Profile</button>
                    </div>
                 </div>
             </div>`;
